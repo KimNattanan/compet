f5304c07d8855d0474800a180a52e8f0a3c7a22b
kim <145192439+KimNattanan@users.noreply.github.com>
2023-10-10 14:56:22 +0700

    Update ioi_prisoner_80.cpp

--- ioi_prisoner/ioi_prisoner_80.cpp
+++ ioi_prisoner/ioi_prisoner_80.cpp
@@ -1,120 +1,63 @@
-//#include "prison.h"
+#include "insects.h"
 #include<bits/stdc++.h>
+
 using namespace std;
-//#define endl '\n'
-#define ll long long
-#define pii pair<int,int>
-#define f first
-#define s second
-#define pb push_back
 
-//#define int ll
+#define in move_inside
+#define press press_button
+#define out move_outside
 
-const int md=1e9+7;
-const int inf=1e9+7;
+set<int> mp,mp2,mp3;
+int n,p;
+vector<int> idx;
 
-vector<vector<int>> ans(61);
-int n;
-
-void playB(int ii,int al,int ar,int bl,int br);
-void playA(int ii,int al,int ar,int bl,int br){
-    if(al==ar&&bl==br) return;
-    if(al>ar||bl>br) return;
-    ans[ii][0]=0;
-    for(int i=al;i<=ar;++i){
-        if(i>br) ans[ii][i]=-2;
-        else if(i<bl) ans[ii][i]=-1;
+bool check(int k1){
+    stack<int> st;
+    for(auto &i:idx){
+        if(mp.size()==p*k1) break;
+        if(mp.count(i)) continue;
+        if(mp3.count(i)) continue;
+        in(i);
+        if(press()>k1) out(i);
+        else mp.insert(i),st.push(i);
     }
-    n=max(n,ii+1);
-    if(bl==br) return;
-    int mid=bl+1+(br-2-bl>>1);
-    int temp=ii+1;
-    if(ii%2==1) temp+=2;
-    for(int i=bl+1;i<=mid;++i) ans[ii][i]=ii+2;
-    for(int i=mid+1;i<=br-1;++i) ans[ii][i]=temp;
-    ans[ii][bl]=-1;
-    ans[ii+2][bl]=-2;
-    ans[temp][bl]=-2;
-    ans[ii][br]=-2;
-    ans[ii+2][br]=-1;
-    ans[temp][br]=-1;
-    n=max(n,max(ii+2,temp)+1);
-    ans[ii+2][0]=ans[temp][0]=1;
-    if(bl+1>=br-1) return;
-    playB(ii+2,bl+1,mid,bl+1,br-1);
-    playB(temp,mid+1,br-1,bl+1,br-1);
-}
-void playB(int ii,int al,int ar,int bl,int br){
-    if(al==ar&&bl==br) return;
-    if(al>ar||bl>ar) return;
-    ans[ii][0]=1;
-    for(int i=bl;i<=br;++i){
-        if(i>ar) ans[ii][i]=-1;
-        else if(i<al) ans[ii][i]=-2;
+    if(mp.size()==p*k1){
+        for(auto &e:mp) if(mp2.count(e)) mp2.erase(e);
+        return 1;
     }
-    n=max(n,ii+1);
-    if(al==ar) return;
-    int mid=al+1+(ar-2-al>>1);
-    int temp=ii+1;
-    if(ii%2==1) temp+=2;
-    for(int i=al+1;i<=mid;++i) ans[ii][i]=ii+2;
-    for(int i=mid+1;i<=ar-1;++i) ans[ii][i]=temp;
-    ans[ii][al]=-2;
-    ans[ii+2][al]=-1;
-    ans[temp][al]=-1;
-    ans[ii][ar]=-1;
-    ans[ii+2][ar]=-2;
-    ans[temp][ar]=-2;
-    n=max(n,max(ii+2,temp)+1);
-    ans[ii+2][0]=ans[temp][0]=0;
-    if(al+1>=ar-1) return;
-    playA(ii+2,al+1,ar-1,al+1,mid);
-    playA(temp,al+1,ar-1,mid+1,ar-1);
-}
-
-vector<vector<int>> devise_strategy(int N){
-    ans.resize(64);
-    for(auto &e:ans) e.resize(N+1);
-    n=3;
-    int mid=2+(N-2>>1);
-    for(int i=2;i<=mid;++i) ans[0][i]=1;
-    for(int i=mid+1;i<=N-1;++i) ans[0][i]=2;
-    ans[0][1]=-1;
-    ans[0][N]=-2;
-    ans[1][1]=-2;
-    ans[1][N]=-1;
-    ans[2][1]=-2;
-    ans[2][N]=-1;
-    ans[1][0]=ans[2][0]=1;
-    if(N!=2){
-        playB(1,2,mid,2,N-1);
-        playB(2,mid+1,N-1,2,N-1);
+    stack<int> st2;
+    for(auto &e:mp2){
+        if(!mp.count(e)) st2.push(e);
     }
-    cout<<n<<" ====="<<endl;
-    ans.resize(n);
-    return ans;
-}
-
-int32_t main(){
-    ios::sync_with_stdio(false); cin.tie(0);
-
-    vector<vector<int>> ans;
-    int n; cin>>n;
-    ans=devise_strategy(n);
-    cout<<"[\n";
-    for(int i=0;i<1;++i){
-        cout<<"[";
-        auto e=ans[(ans.size()-1)-2];
-        for(auto &f:e) cout<<f<<" ";
-        cout<<"]\n";
+    while(!st2.empty()){
+        mp2.erase(st2.top());
+        mp3.insert(st2.top());
+        st2.pop();
     }
-    cout<<"]";
-
+    while(st.size()){
+        mp.erase(st.top());
+        out(st.top());
+        st.pop();
+    }
     return 0;
 }
-/**
 
+int min_cardinality(int N){
+    n=N;
+    for(int i=0;i<n;++i){
+        idx.push_back(i);
+        in(i);
+        if(press()>1) out(i),mp2.insert(i);
+        else mp.insert(i);
+    }
+    p=mp.size();
 
-
-*/
-
+    int l=1,r=(n)/p;
+    while(l<r){
+        int mid=l+(r-l+1>>1);
+        random_shuffle(idx.begin(),idx.end());
+        if(check(mid)) l=mid;
+        else r=mid-1;
+    }
+    return l;
+}
