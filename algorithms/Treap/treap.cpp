a269df23cd8dae3d39029c27f160f8d014e2bb0a
kim <145192439+KimNattanan@users.noreply.github.com>
2023-11-02 13:10:53 +0700

    Update treap.cpp

--- algorithms/Treap/treap.cpp
+++ algorithms/Treap/treap.cpp
@@ -8,22 +8,23 @@ struct treap{
         char val;
         int cnt,prio;
         node *l,*r;
-        bool lazy;
-        node(char val_):val(val_),cnt(1),prio(rnd()),l(nullptr),r(nullptr),lazy(0){}
+        node(char val_):val(val_),cnt(1),prio(rnd()),l(nullptr),r(nullptr){}
     };
     using pitem=node*;
     pitem rt;
     int getCnt(pitem t){return t?t->cnt:0;}
-    void setCnt(pitem t){t->cnt=getCnt(t->l)+getCnt(t->r);}
+    void updCnt(pitem t){if(t)t->cnt=getCnt(t->l)+getCnt(t->r)+1;}
     void split(pitem t,pitem &l,pitem &r,int x){
         if(!t) return void(l=r=nullptr);
         if(getCnt(t->l)>=x) split(t->l,l,t->l,x),r=t;
         else split(t->r,t->r,r,x-getCnt(t->l)-1),l=t;
+        updCnt(t);
     }
     void merge(pitem &t,pitem l,pitem r){
         if(!l||!r) return void(l?t=l:t=r);
         if(l->prio>r->prio) merge(l->r,l->r,r),t=l;
         else merge(r->l,l,r->l),t=r;
+        updCnt(t);
     }
     void insert(int x,string s){
         pitem l,r;
@@ -31,17 +32,16 @@ struct treap{
         for(auto &e:s) merge(l,l,new node(e));
         merge(rt,l,r);
     }
+    string print(){
+        string s;
+        function<void(pitem)> dfs=[&](pitem t){
+            if(!t) return;
+            dfs(t->l);
+            s.push_back(t->val);
+            dfs(t->r);
+        };
+        dfs(rt);
+        return s;
+    }
 }t;
 
-int32_t main(){
-    ios::sync_with_stdio(false); cin.tie(0);
-
-    string s; cin>>s;
-    int n=s.length();
-    t.insert(0,s);
-
-    return 0;
-}
-/*
-
-*/
