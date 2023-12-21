05f62a8e1fb11163d37cf37ad5ff2953e62e1f4e
kim <145192439+KimNattanan@users.noreply.github.com>
2023-12-21 15:42:49 +0700

    Create fat_node.cpp

---
+++ algorithms/Persistent_array/fat_node.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define eb emplace_back
+using pii=pair<int,int>;
+#define f first
+#define s second
+
+struct persist{
+	vector<vector<pii>> vec;
+	void init(int n){
+		vec=vector<vector<pii>>(n,vector<pii>(1));
+	}
+	void add(int i,int t,int x){
+		vec[i].eb(t,x+vec[i].back().s);
+	}
+	int qr(int i,int t){
+		int id=upper_bound(vec[i].begin(),vec[i].end(),pii(t,INT_MAX))-vec[i].begin()-1;
+		return vec[i][id].s;
+	}
+};
