6a0a951fcdfced40f45141b379d2e928367535c5
kim <145192439+KimNattanan@users.noreply.github.com>
2024-01-14 22:34:38 +0700

    Update 0003.cpp

--- beta/0003.cpp
+++ beta/0003.cpp
@@ -6,7 +6,7 @@ template<class T>
 struct Matrix{
     int n,m;
     vector<vector<T>> a;
-    Matrix(const int &n,const int &m):n(n),m(m),a(n,vector<T>(m)){}
+    Matrix(const int &n=0,const int &m=0):n(n),m(m),a(n,vector<T>(m)){}
     Matrix& operator+=(const Matrix &r){
         for(int i=0;i<n;++i) for(int j=0;j<m;++j) a[i][j]+=r.a[i][j];
         return *this;
@@ -25,13 +25,27 @@ struct Matrix{
     }
 };
 
+struct _0003{
+    int n,m;
+    using matrix=Matrix<ll>;
+    matrix m1,m2;
+    friend istream& operator>>(istream &is,_0003 &x){
+        is>>x.n>>x.m;
+        x.m1=x.m2=matrix(x.n,x.m);
+        cin>>x.m1>>x.m2;
+        return is;
+    }
+    friend ostream& operator<<(ostream &os,const _0003 &x){
+        os<<(x.m1+x.m2);
+        return os;
+    }
+}solution;
+
 int main(){
     ios::sync_with_stdio(false);cin.tie(0);
 
-    int n,m; cin>>n>>m;
-    Matrix<ll> m1(n,m),m2(n,m);
-    cin>>m1>>m2;
-    cout<<(m1+m2);
+    cin>>solution;
+    cout<<solution;
 
     return 0;
 }
