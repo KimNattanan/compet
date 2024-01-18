371e718266663f251f34b26e6c4968bf71e09ba3
kim <145192439+KimNattanan@users.noreply.github.com>
2024-01-18 22:26:43 +0700

    Update tumso20_space2.cpp

--- beta/tumso20_space2.cpp
+++ beta/tumso20_space2.cpp
@@ -14,16 +14,16 @@ int H[100005];
 
 struct segment1{
     vector<segment1> child;
-    int l,r,mid,mx;
-    segment1(int l=0,int r=0):l(l),r(r),mid(l+(r-l>>1)){}
+    int l,r,mx;
+    segment1(int l=0,int r=0):l(l),r(r){}
     void build(){
         if(l==r) return void(mx=l);
-        child.eb(l,mid),child.eb(mid+1,r);
+        child.eb(l,(l+r>>1)),child.eb((l+r>>1)+1,r);
         child[0].build(),child[1].build();
         if(H[child[0].mx]>H[child[1].mx]) mx=child[0].mx;
         else mx=child[1].mx;
     }
-    int qr(int l0,int r0){
+    int qr(int &l0,int &r0){
         if(l0<=l&&r<=r0) return mx;
         if(l>r0||r<l0) return 0;
         int il=child[0].qr(l0,r0);
@@ -44,62 +44,57 @@ struct segment2{
     using pnode=node*;
     vector<pnode> rt;
     int l0,r0;
-    void build(pnode &t,const int &il,const int &ir){
+    void build(pnode &t,int il,int ir){
         t=new node();
         if(il==ir) return;
-        int mid=il+(ir-il>>1);
-        build(t->L,il,mid),build(t->R,mid+1,ir);
+        build(t->L,il,(il+ir>>1)),build(t->R,(il+ir>>1)+1,ir);
     }
     void build(int n,int l,int r){
         rt=vector<pnode>(n+1);
         l0=l,r0=r;
         build(rt[0],l,r);
     }
-    void flush(pnode t,const int &il,const int &ir){
+    void flush(pnode t,int &il,int &ir){
         if(t&&t->lz){
-            t->sum+=(ir-il+1)*t->lz;
             if(il!=ir){
-                int mid=il+(ir-il>>1);
                 t->L=new node(*(t->L));
                 t->R=new node(*(t->R));
                 t->L->lz+=t->lz;
                 t->R->lz+=t->lz;
             }
+            t->sum+=(ir-il+1)*t->lz;
             t->lz=0;
         }
     }
-    void upd(pnode t0,pnode &t1,const int &il,const int &ir,const int &l,const int &r,const int &x){
+    void upd(pnode t0,pnode &t1,int il,int ir,int &l,int &r){
         flush(t0,il,ir);
         if(il>r||ir<l) return void(t1=t0);
         if(l<=il&&ir<=r){
             t1=new node(*t0);
-            t1->lz+=x;
+            ++t1->lz;
             flush(t1,il,ir);
             return;
         }
         t1=new node();
-        int mid=il+(ir-il>>1);
-        upd(t0->L,t1->L,il,mid,l,r,x);
-        upd(t0->R,t1->R,mid+1,ir,l,r,x);
+        upd(t0->L,t1->L,il,(il+ir>>1),l,r);
+        upd(t0->R,t1->R,(il+ir>>1)+1,ir,l,r);
         t1->sum=t1->L->sum+t1->R->sum;
     }
-    void upd(const int &prev,const int &now,const int &l,const int &r,const int &x){upd(rt[prev],rt[now],l0,r0,l,r,x);}
-    ll qr(pnode t,const int &il,const int &ir,const int &l,const int &r){
+    void upd(int prev,int now,int &l,int &r){upd(rt[prev],rt[now],l0,r0,l,r);}
+    ll qr(pnode t,int il,int ir,int &l,int &r){
         flush(t,il,ir);
         if(l<=il&&ir<=r) return t->sum;
         if(il>r||ir<l) return 0;
-        int mid=il+(ir-il>>1);
-        return qr(t->L,il,mid,l,r)+qr(t->R,mid+1,ir,l,r);
+        return qr(t->L,il,(il+ir>>1),l,r)+qr(t->R,(il+ir>>1)+1,ir,l,r);
     }
-    ll qr(const int &now,const int &l,const int &r){return qr(rt[now],l0,r0,l,r);}
+    ll qr(int now,int l,int r){return qr(rt[now],l0,r0,l,r);}
 }t2;
 
-ll play(int l,int r,int h){
+ll play(int l,int r,int &h){
     if(l>r) return 1;
     int mid=t1.qr(l,r);
-    ll cnt=0;
-    if(H[mid]<h) cnt=(t2.qr(r,H[mid]+1,h)-t2.qr(l-1,H[mid]+1,h))%md;
-    return (cnt+(play(l,mid-1,H[mid])*play(mid+1,r,H[mid]))%md)%md;
+    if(H[mid]>=h) return (play(l,mid-1,H[mid])*play(mid+1,r,H[mid]))%md;
+    return ((t2.qr(r,H[mid]+1,h)-t2.qr(l-1,H[mid]+1,h))%md+(play(l,mid-1,H[mid])*play(mid+1,r,H[mid]))%md)%md;
 }
 
 int main(){
@@ -108,14 +103,12 @@ int main(){
     int n; cin>>n;
     for(int i=1;i<=n;++i) cin>>H[i];
     int mx=0;
-    vector<pii> vec;
+    t2.build(n+5,1,MXR);
     for(int i=1;i<=n;++i){
         int l,r; cin>>l>>r;
-        vec.eb(l,r);
+        t2.upd(i-1,i,l,r);
         mx=max(mx,r);
     }
-    t2.build(n+5,1,mx);
-    for(int i=1;i<=n;++i) t2.upd(i-1,i,vec[i-1].f,vec[i-1].s,1);
     t1=segment1(1,n);
     t1.build();
     cout<<play(1,n,mx);
