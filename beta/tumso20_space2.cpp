2d346888a8fe5447d9e6d2bf7cd3aa3d2ed44627
kim <145192439+KimNattanan@users.noreply.github.com>
2024-01-24 01:40:17 +0700

    Create tumso20_space2.cpp

---
+++ beta/tumso20_space2.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using ld=long double;
+#define eb emplace_back
+using pii=pair<int,int>;
+#define f first
+#define s second
+
+const ll md=998244353;
+const int sq=1000;
+// int sq;
+
+int n,H[100005],lm[100005],rm[100005];
+vector<pii> vec;
+
+struct segment{
+    vector<segment> child;
+    int l,r,sum,lz;
+    segment(const int &l=0,const int &r=0):l(l),r(r),sum(0),lz(0){}
+    void flush(){
+        if(l!=r&&child.empty()){
+            int mid=l+(r-l>>1);
+            child.eb(l,mid),child.eb(mid+1,r);
+            child[0].lz=child[1].lz=sum/(r-l+1);
+        }
+        if(lz){
+            if(l!=r){
+                child[0].lz+=lz;
+                child[1].lz+=lz;
+            }
+            sum+=lz*(r-l+1);
+            lz=0;
+        }
+    }
+    void add(int &l0,int &r0){
+        flush();
+        if(l0<=l&&r<=r0){
+            lz+=1;
+            flush();
+            return;
+        }
+        if(l>r0||r<l0) return;
+        child[0].add(l0,r0),child[1].add(l0,r0);
+        sum=child[0].sum+child[1].sum;
+    }
+    int qr(int l0,int r0){
+        flush();
+        if(l0<=l&&r<=r0) return sum;
+        if(l>r0||r<l0) return 0;
+        return child[0].qr(l0,r0)+child[1].qr(l0,r0);
+    }
+}t[320];
+
+ll play(int l,int r,int mid,int &h){
+    if(l>r) return 1;
+    if(H[mid]>=h) return (play(l,mid-1,lm[mid],H[mid])*play(mid+1,r,rm[mid],H[mid]))%md;
+    ll cnt=0;
+    int bl=l/sq;
+    int br=r/sq;
+    for(int i=bl;i<=br;++i) cnt+=t[i].qr(H[mid]+1,h);
+    for(int i=l-1;i>=1&&i/sq==bl;--i) cnt-=max(0,min(h,vec[i].s)-max(H[mid],vec[i].f-1));
+    for(int i=r+1;i<=n&&i/sq==br;++i) cnt-=max(0,min(h,vec[i].s)-max(H[mid],vec[i].f-1));
+    return (cnt%md+(play(l,mid-1,lm[mid],H[mid])*play(mid+1,r,rm[mid],H[mid]))%md)%md;
+}
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);
+
+    cin>>n;
+    // sq=sqrt(n);
+    for(int i=1;i<=n;++i) cin>>H[i];
+    int mx=0;
+    vec=vector<pii>(n+1);
+    for(int i=1;i<=n;++i){
+        cin>>vec[i].f>>vec[i].s;
+        mx=max(mx,vec[i].s);
+    }
+    for(int i=n/sq;i>=0;--i) t[i]=segment(1,mx);
+    for(int i=1;i<=n;++i) t[i/sq].add(vec[i].f,vec[i].s);
+
+    H[0]=1e9;
+    stack<int> st;
+    st.emplace(0);
+    for(int i=1;i<=n;++i){
+        while(H[st.top()]<H[i]) st.pop();
+        lm[i]=rm[st.top()];
+        rm[st.top()]=i;
+        st.emplace(i);
+    }
+
+    cout<<play(1,n,rm[0],mx);
+
+    return 0;
+}
