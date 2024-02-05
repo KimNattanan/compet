4e4fc90f5ef6c3cf0aa60b89e349e526875bf31e
kim <145192439+KimNattanan@users.noreply.github.com>
2024-02-05 18:10:54 +0700

    Update encoder.cpp

--- ioi/ioi11_parrots/sub1_4/encoder.cpp
+++ ioi/ioi11_parrots/sub1_4/encoder.cpp
@@ -2,9 +2,15 @@
 #include "encoderlib.h"
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 void encode(int N, int M[])
 {
-  for(int i=0;i<N;++i) M[i]=0;
-  for(int i=0;i<N;++i) send(0);
+  for(int i=0;i<N;++i){
+    vector<int> vec(9,i);
+    int x=M[i];
+    for(int j=x/100-1, mask=1<<7 ;j>=0;--j,x-=100) vec[j]+=mask;
+    for(int j=x/10-1, mask=1<<6 ;j>=0;--j,x-=10) vec[j]+=mask;
+    for(int j=x-1, mask=1<<5 ;j>=0;--j,x-=1) vec[j]+=mask;
+    for(auto &e:vec) send(e);
+  }
 }
