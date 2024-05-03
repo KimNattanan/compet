ed5d876990878c2f54e1647c2c62bc1ad3baa027
kim <145192439+KimNattanan@users.noreply.github.com>
2024-05-03 22:08:08 +0700

    Update runthis.sh

--- Kim's Problem/runthis.sh
+++ Kim's Problem/runthis.sh
@@ -1,4 +1,5 @@
 (
+cd C:\...\download
 g++ runthis.cpp -o runthis.exe
-for /R C:\Users\Nattanan\Documents\.me\cpp\program\program1\tests\ %G in (*) do runthis.exe %G false
+for /R C:\...\tests\ %G in (*) do runthis.exe %G false
 )
