ee2f3dee40f9fe3acf8e5d031cc0a9a3843475d9
kim <145192439+KimNattanan@users.noreply.github.com>
2024-05-06 21:20:25 +0700

    Create toi16_dinocell.cpp

---
+++ beta/toi16_dinocell.cpp
@@ -0,0 +1,36 @@
+/*
+    Toi16_DinoCell
+*/
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int a[10000005],k;
+int f(int x){
+    return a[k]*(x/k)+a[x%k];
+}
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(0);
+
+    int z,n; cin>>z>>k>>n;
+    for(int i=2;i<=k;++i){
+        if(a[i] || k%i) continue;
+        for(int j=i;j<=k;j+=i) a[j]=1;
+    }
+    for(int i=1;i<=k;++i) a[i]=(!a[i])+a[i-1];
+
+    ll mn,mx,ans=1;
+    for(int i=1;i<=n;++i){
+        int x; cin>>x;
+        if(i==1) mn=mx=2LL*f(x-1)-x;
+        else{
+            ans=max({ans,abs(x-2LL*f(x)+1+mx),abs(x-2LL*f(x)+1+mn)});
+            mn=min(mn,2LL*f(x-1)-x);
+            mx=max(mx,2LL*f(x-1)-x);
+        }
+    }
+    cout<<ans;
+
+	return 0;
+}
