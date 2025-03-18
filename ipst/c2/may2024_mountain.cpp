b06877e000c183de502e52d27afe327c4bcb13b1
kim <145192439+KimNattanan@users.noreply.github.com>
2025-03-18 13:41:14 +0700

    Update may2024_mountain.cpp

--- ipst/c2/may2024_mountain.cpp
+++ ipst/c2/may2024_mountain.cpp
@@ -1,115 +1,117 @@
+#include "mountain.h"
 #include<bits/stdc++.h>
 using namespace std;
-using pii=pair<int,int>;
 using ll=long long;
+using pii=pair<int,int>;
 #define f first
 #define s second
 #define eb emplace_back
-#include "mountain.h"
+#define sz(x) (int)x.size()
+#define add(x,y) ((x+y)%md)
+#define Add(x,y) (x=add(x,y))
+#define mul(x,y) ((x*y)%md)
+#define Mul(x,y) (x=mul(x,y))
+const int inf=1e9;
+const ll linf=1e18;
+const ll md=1e9+7;
 
-ll a[100005];
-
-struct segment{
-	ll sum[1<<18];
+struct Segment{
+	ll a[1<<18],*qs;
 	int lz[1<<18],l0,r0;
-	void build(int l,int r){ l0=l,r0=r; }
+	void init(int l,int r,ll qs_[]){ l0=l,r0=r,qs=qs_; }
 	void flush(int i,int il,int ir){
 		if(!lz[i]) return;
 		if(il!=ir) lz[i<<1]+=lz[i], lz[i<<1|1]+=lz[i];
-		sum[i]+=lz[i]*(a[ir]-a[il-1]);
+		a[i]+=lz[i]*(qs[ir]-qs[il-1]);
 		lz[i]=0;
 	}
 	void upd(int i,int il,int ir,int l,int r,int x){
 		flush(i,il,ir);
-		if(l<=il&&ir<=r) return lz[i]+=x, flush(i,il,ir);
 		if(il>r||ir<l) return;
+		if(l<=il&&ir<=r) return lz[i]+=x, flush(i,il,ir);
 		int mid=il+ir>>1;
 		upd(i<<1,il,mid,l,r,x), upd(i<<1|1,mid+1,ir,l,r,x);
-		sum[i]=sum[i<<1]+sum[i<<1|1];
-	}
-	ll qr(int i,int il,int ir,int l,int r){
-		flush(i,il,ir);
-		if(l<=il&&ir<=r) return sum[i];
-		if(il>r||ir<l) return 0LL;
-		int mid=il+ir>>1;
-		return qr(i<<1,il,mid,l,r)+qr(i<<1|1,mid+1,ir,l,r);
+		a[i]=a[i<<1]+a[i<<1|1];
 	}
 	void upd(int l,int r,int x){ upd(1,l0,r0,l,r,x); }
+	ll qr(int i,int il,int ir,int l,int r){
+		flush(i,il,ir);
+		if(il>r||ir<l) return 0;
+		if(l<=il&&ir<=r) return a[i];
+		int mid=il+ir>>1;
+		return qr(i<<1,il,mid,l,r) + qr(i<<1|1,mid+1,ir,l,r);
+	}
 	ll qr(int l,int r){ return qr(1,l0,r0,l,r); }
 };
 
-
-int lh[100005],rh[100005];
-vector<int> adj[100005],A,C;
-
-
-int p[100005],big[100005],head[100005],tour[100005],id;
-int dfs(int u,int p){
-	int szu=1,mx=0;
-	::p[u]=p;
-	big[u]=-1;
-	for(auto &v:adj[u]){
-		int szv=dfs(v,u);
-		szu+=szv;
-		if(szv>mx) mx=szv, big[u]=v;
+int tl[100005],tr[100005],rt;
+struct HLD{
+	int sz[100005],pa[100005],big[100005],op[100005],ed[100005],h[100005],id;
+	ll qs[100005];
+	Segment t[2];
+	void dfs(int u,int p){
+		big[u]=-1;
+		sz[u]=1;
+		pa[u]=p;
+		for(auto &v:{tl[u],tr[u]})if(v!=-1){
+			dfs(v,u);
+			sz[u]+=sz[v];
+			if(big[u]==-1 || sz[big[u]]<sz[v]) big[u]=v;
+		}
 	}
-	return szu;
-}
-void decomp(int u,int h){
-	tour[u]=++id;
-	head[u]=h;
-	a[id]=a[id-1]+A[u]-(p[u]==-1 ? 0 : A[p[u]]);
-	if(big[u]!=-1) decomp(big[u],h);
-	for(auto &v:adj[u]) if(v!=big[u]) decomp(v,v);
-}
+	void decomp(int u,int p){
+		op[u]=++id;
+		h[u]=p;
+		if(big[u]!=-1) decomp(big[u],p);
+		for(auto &v:{tl[u],tr[u]}) if(v!=-1 && v!=big[u]) decomp(v,v);
+		ed[u]=id;
+	}
+	void init(int rt,vector<int> &A){
+		dfs(rt,-1), decomp(rt,rt);
+		t[0].init(1,id,qs), t[1].init(1,id,qs);
+		for(int i=0;i<sz(A);++i) qs[op[i]]=A[i]-(pa[i]==-1? 0 : A[pa[i]]);
+		for(int i=1;i<=id;++i) qs[i]+=qs[i-1];
+	}
+	void upd(int u,int c,ll x){
+		t[c-1].upd(op[h[u]],op[u], x);
+		if(pa[h[u]]!=-1) upd(pa[h[u]],c,x);
+	}
+	ll qr(int u,int c){
+		if(pa[h[u]]!=-1) return t[c-1].qr(op[h[u]],op[u]) + qr(pa[h[u]],c);
+		return t[c-1].qr(op[h[u]],op[u]);
+	}
+}hld;
 
-
-ll cost;
-segment t_red,t_blue;
-void add_red(int u,int x){
-	t_red.upd(tour[head[u]],tour[u],x);
-	cost+=x*t_blue.qr(tour[head[u]],tour[u]);
-	if(p[head[u]]!=-1) add_red(p[head[u]],x);
+int col[100005];
+ll ans;
+void set_color(int u,int c){
+	if(col[u]==c) return;
+	if(col[u]){
+		hld.upd(u,col[u],-1);
+		ans-=hld.qr(u,3-col[u]);
+	}
+	if(!(col[u]=c)) return;
+	hld.upd(u,c,1);
+	ans+=hld.qr(u,3-c);
 }
-void add_blue(int u,int x){
-	t_blue.upd(tour[head[u]],tour[u],x);
-	cost+=x*t_red.qr(tour[head[u]],tour[u]);
-	if(p[head[u]]!=-1) add_blue(p[head[u]],x);
-}
-
 
 void initialize(int N, std::vector<int> A, std::vector<int> C) {
-	::A=A, ::C=C;
+	// Fill your code here
 	stack<int> st;
-	int rt=-1;
-	for(int i=0;i<N;++i) lh[i]=rh[i]=-1;
 	for(int i=0;i<N;++i){
-		while(!st.empty()&&A[st.top()]<A[i]) st.pop();
-		if(rt==-1) rt=i;
-		else if(st.empty()) lh[i]=rt, rt=i;
-		else lh[i]=rh[st.top()], rh[st.top()]=i;
+		tl[i]=tr[i]=-1;
+		while(!st.empty() && A[i]>A[st.top()]) tl[i]=st.top(), st.pop();
+		if(st.empty()) rt=i;
+		else tr[st.top()]=i;
 		st.emplace(i);
 	}
-	for(int i=0;i<N;++i){
-		if(lh[i]!=-1) adj[i].eb(lh[i]);
-		if(rh[i]!=-1) adj[i].eb(rh[i]);
-	}
-	dfs(rt,-1);
-	decomp(rt,rt);
-	t_red.build(1,id), t_blue.build(1,id);
-	for(int i=0;i<N;++i){
-		if(C[i]==1) add_red(i,1);
-		else if(C[i]==2) add_blue(i,1);
-	}
-	// Fill your code here
+	hld.init(rt,A);
+	for(int i=0;i<N;++i) set_color(i,C[i]);
+	return ;
 }
 
 long long race_cost(int S, int X) {
-	if(C[X]==1) add_red(X,-1);
-	else if(C[X]==2) add_blue(X,-1);
-	if(S==1) add_red(X,1);
-	else if(S==2) add_blue(X,1);
-	C[X]=S;
-	return cost;
 	// Fill your code here
+	set_color(X,S);
+	return ans;
 }
