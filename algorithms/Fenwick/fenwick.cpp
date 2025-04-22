ca2bc0873e8dce74f1c0cc0ea0888b598e0bbe0f
kim <145192439+KimNattanan@users.noreply.github.com>
2025-04-22 10:18:13 +0700

    Update fenwick.cpp

--- algorithms/Fenwick/fenwick.cpp
+++ algorithms/Fenwick/fenwick.cpp
@@ -1,25 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll=long long;
+#define sz(x) (int)x.size()
 
-struct fenwick{
-    vector<int> bit;
-    fenwick(int n=0){init(n);}
-    void init(int n){bit.assign(n,0);}
-    void upd(int i,int x){
-        for(;i<bit.size();i+=i&-i) bit[i]+=x;
+struct Fenwick{
+  vector<ll> bit;
+  void init(int n){ bit.assign(n,0); }
+  void upd(int i,ll x){ for(;i<sz(bit); i+=i&-i) bit[i]+=x; }
+  ll qr(int i){
+    ll res=0;
+    for(;i>0;i-=i&-i) res+=bit[i];
+    return res;
+  }
+  int ub(ll x){ // lowest i  of which  sum(1->i) > x
+    int pos=0;
+    ll sum=0;
+    for(int i=1<<31-__builtin_clz(sz(bit));i>0;i>>=1){
+      if(pos+i<sz(bit) && sum+bit[pos+i] <= x) sum += bit[pos+=i];
     }
-    int qr(int i){
-        int ret=0;
-        for(;i>0;i-=i&-i) ret+=bit[i];
-        return ret;
+    return pos+1;
+  }
+}fw;
+
+struct Fenwick2D{
+  vector<vector<ll>> bit;
+  int n,m;
+  void init(int n,int m){ bit.assign(n,vector<ll>(m,0)); }
+  void upd(int i0,int j0,ll x){
+    for(int i=i0;i<sz(bit);i+=i&-i){
+      for(int j=j0;j<sz(bit[0]);j+=j&-j) bit[i][j]+=x;
     }
-    void upd(int l,int r,int x){ upd(l,x), upd(r+1,-x); }
-    int qr(int l,int r){ return qr(r)-qr(l-1); }
-    int search(int x)
-        int pos=0,sum=0;
-        for(int i=1<<31-__builtin_clz((int)bit.size()-1);i>0;i>>=1){
-            if(pos+i<bit.size()&&sum+bit[pos+i]<x) pos+=i,sum+=bit[pos];
-        }
-        return pos+1;
+  }
+  ll qr(int i0,int j0){
+    ll res=0;
+    for(int i=i0;i>0;i-=i&-i){
+      for(int j=j0;j>0;j-=j&-j) res+=bit[i][j];
     }
-};
+    return res;
+  }
+}fw2;
