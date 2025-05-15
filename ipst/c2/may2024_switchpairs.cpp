db3dace57b1e149668c8aa2227ecbaf283fc7971
kim <145192439+KimNattanan@users.noreply.github.com>
2025-05-15 12:01:05 +0700

    Update may2024_switchpairs.cpp

--- ipst/c2/may2024_switchpairs.cpp
+++ ipst/c2/may2024_switchpairs.cpp
@@ -1,89 +1,87 @@
 #include "switchpairs.h"
 #include<bits/stdc++.h>
 using namespace std;
-using pii=pair<int,int>;
-using ll=long long;
-#define f first
-#define s second
 #define eb emplace_back
+#define sz(x) (int)x.size()
 
-int timer=0;
-const int MX_timer=15000;
-int ask(vector<int> vec){
-	if(++timer>MX_timer) assert(0);
-	return control_switches(vec);
+vector<int> v0,v;
+int M;
+int ask(vector<int> v){
+  for(int i=0;i<sz(v);++i) if(v0[i]!=-1) v[i]=v0[i];
+  return control_switches(v);
 }
 
-int N,N2;
-vector<int> unused;
-vector<pii> ans;
-vector<int> fix(vector<int> vec){
-	for(auto &[a,x]:ans) vec[a]=x;
-	return vec;
-}
-void insert_ans(int a,int x){
-	ans.eb(a,x);
-	unused.erase(lower_bound(unused.begin(),unused.end(),a));
-}
-
-void all(bool x,int now){
-	vector<int> vec(N2);
-	int l=0, r=unused.size()-1;
-	while(l<r){
-		int mid=l+r>>1;
-		for(int i=0;i<unused.size();++i) vec[unused[i]]=x^(i>mid);
-		if(ask(fix(vec))>now) r=mid;
-		else l=mid+1;
-	}
-	insert_ans(unused[l],x);
-	l=0, --r;
-	while(l<r){
-		int mid=l+r+1>>1;
-		for(int i=0;i<unused.size();++i) vec[unused[i]]=x^(i<mid);
-		if(ask(fix(vec))>now) l=mid;
-		else r=mid-1;
-	}
-	insert_ans(unused[l],x);
-}
-
-void solve(int N_){
-	N=N_, N2=N<<1;
-	vector<int> vec(N2),_1(N2,1),_0(N2,0);
-	unused.resize(N2);
-	iota(unused.begin(),unused.end(),0);
-	vector<int> bits(10);
-	for(int now=0;now<N;++now){
-		if(ask(fix(_0))>now){ all(0,now); continue; }
-		if(ask(fix(_1))>now){ all(1,now); continue; }
-		int bit,msk,state;
-		iota(bits.begin(),bits.end(),0);
-		shuffle(bits.begin(),bits.end(),mt19937(0));
-		for(auto &e:bits){
-			bit=e, msk=1<<e;
-			state=0;
-			for(int j=0;j<N2;++j) vec[j]=bool(j&msk);
-			if(ask(fix(vec))>now) break;
-			state=1;
-			if(e==bits.back()) break;
-			for(int j=0;j<N2;++j) vec[j]=!vec[j];
-			if(ask(fix(vec))>now) break;
-		}
-		int l=0, r=unused.size()-1;
-		while(l<r){
-			int mid=l+r>>1;
-			for(int i=0;i<unused.size();++i) vec[unused[i]]=(i>mid)^state^bool(unused[i]&msk);
-			if(ask(fix(vec))>now) r=mid;
-			else l=mid+1;
-		}
-		insert_ans(unused[l],state^bool(unused[l]&msk));
-		l=0, --r;
-		while(l<r){
-			int mid=l+r+1>>1;
-			for(int i=0;i<unused.size();++i) vec[unused[i]]=(i<mid)^state^bool(unused[i]&msk);
-			if(ask(fix(vec))>now) l=mid;
-			else r=mid-1;
-		}
-		insert_ans(unused[l],state^bool(unused[l]&msk));
-	}
-	ask(fix(vec));
+void solve(int N){
+  M=N<<1;
+  int K=31-__builtin_clz(M-1);
+  v0.assign(M,-1);
+  v.resize(M);
+  for(int i=0;i<N;++i){
+    int y=-1,x0,x1;
+    fill(v.begin(),v.end(),0);
+    if(ask(v)>i) y=0;
+    else{
+      fill(v.begin(),v.end(),1);
+      if(ask(v)>i) y=1;
+    }
+    if(y==-1){
+      vector<int> y0,y1;
+      for(int j=0;j<=K;++j){
+        for(int k=0;k<M;++k) v[k]=bool(k&1<<j);
+        if(ask(v)>i){ // 1:j 0:no-j
+          for(int k=0;k<M;++k){
+            if(v[k]) y1.eb(k);
+            else y0.eb(k);
+          }
+          break;
+        }
+        for(auto &e:v) e=!e;
+        if(ask(v)>i){ // 1:no-j 0:j
+          for(int k=0;k<M;++k){
+            if(v[k]) y1.eb(k);
+            else y0.eb(k);
+          }
+          break;
+        }
+      }
+      int l=0,r=sz(y0)-1;
+      while(l<r){
+        int mid=l+r>>1;
+        fill(v.begin(),v.end(),1);
+        for(int j=0;j<sz(y0);++j) v[y0[j]]=(j<=mid ? 0 : 1);
+        if(ask(v)>i) r=mid;
+        else l=mid+1;
+      }
+      x0=y0[l];
+      l=0,r=sz(y1)-1;
+      while(l<r){
+        int mid=l+r>>1;
+        fill(v.begin(),v.end(),0);
+        for(int j=0;j<sz(y1);++j) v[y1[j]]=(j<=mid ? 1 : 0);
+        if(ask(v)>i) r=mid;
+        else l=mid+1;
+      }
+      x1=y1[l];
+    }else{
+      int l=0,r=M-1;
+      while(l<r){
+        int mid=l+r>>1;
+        for(int j=0;j<M;++j) v[j]=(j<=mid ? y : !y);
+        if(ask(v)>i) r=mid;
+        else l=mid+1;
+      }
+      x0=l;
+      l=0,r=M-1;
+      while(l<r){
+        int mid=l+r+1>>1;
+        for(int j=0;j<M;++j) v[j]=(j>=mid ? y : !y);
+        if(ask(v)>i) l=mid;
+        else r=mid-1;
+      }
+      x1=l;
+    }
+    if(y==-1) v0[x0]=0, v0[x1]=1;
+    else v0[x0]=v0[x1]=y;
+  }
+  ask(v0);
 }
